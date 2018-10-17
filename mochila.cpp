7bf0c312d5a14a428c87a5a1bc0482cf69899131
alexandrejp1 <santacruzalexandre@gmail.com>
2018-10-17 20:16:26 -0300

    Update mochila.cpp

--- mochila.cpp
+++ mochila.cpp
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 using namespace std;
 
+#include <sys/time.h>  //libs pra stack memory
+#include <sys/resource.h>
 /*
 	n= NUMERO DE ELEMENTOS
     	M = CAPACIDADE DA MOCHILA
@@ -16,14 +18,19 @@ int MochilaInt(int n, int M, int pesos[], int valores[])
 {
     int A[n+1][M+1]; 
 
-   for (int i=0;i<= n;i++)  { 
+   for (int i=0;i<= n;i++)  {
+
+   	cout << pesos[i] << endl; 
+    
        for (int j=0;j<=M;j++)   { 
            if (i==0||j==0) A[i][j] = 0; //condição inicial não temos nada na mochila
            else if (pesos[i-1]<=j) A[i][j] = max(A[i-1][j],valores[i-1] + A[i-1][j-pesos[i-1]]); //ainda dá pra add item na mochila 
            																	//temos 2 condiões: ainda tem espaço ou tentamos retirar um item da mochila pra trocar                                                               
            else A[i][j] = A[i-1][j];  //mochila cheia                                                                                               
        }
-   }
+    }
+
+
 
 for(int x = 0; x <= n; x++){ //exibir a tabela preenchida
 	for(int z = 0; z <= M; z++){
@@ -36,15 +43,34 @@ puts("\n");
 return A[n][M]; //final da matriz (melhor valor)
 }
 
-int main(void)
-{   
+int main(void){   
+
+	const rlim_t kStackSize = 64L * 1024L * 1024L;   // min stack size = 64 Mb
+    struct rlimit rl;
+    int result;
+
+    result = getrlimit(RLIMIT_STACK, &rl);
+    if (result == 0)
+    {
+        if (rl.rlim_cur < kStackSize)
+        {
+            rl.rlim_cur = kStackSize;
+            result = setrlimit(RLIMIT_STACK, &rl);
+            if (result != 0)
+            {
+                fprintf(stderr, "setrlimit returned result = %d\n", result);
+            }
+        }
+    }
+
+
 
 	int ord[1000000];
 	string teste;
-	cout << "Arquivo de teste: ";
+	cout << "Digite o arquivo de teste, exemplo: 'mochila01.txt'" << endl;
 	cin >> teste;
 	ifstream in(teste.c_str());
-	char num[10];
+	char num[1000000];
 	int fim = 0;
 	while(!in.eof()){
 		in >> num;
@@ -56,15 +82,22 @@ int main(void)
 	int M = ord[1];
 	int pesos[n];
 	int valores[n];
-	int count = 0;
+	int j = 0;
 
-	for (int i=2; i < 2*n+2; i = i+2)
-	{
-		pesos[count] = ord[i];
-		valores[count] = ord[i+1];
-		count++;
+	for (int i=2; i < 2*n+2; i = i+2){
+		pesos[j] = ord[i];
+		valores[j] = ord[i+1];
+		j++;
 	}
 
     cout << "Valor ótimo: " <<  MochilaInt(n,M,pesos,valores) << endl;
     return 0;
+
+
+
+
+
+
+
+
 }
